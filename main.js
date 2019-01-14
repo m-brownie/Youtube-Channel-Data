5120c93a05bbf59d08e4fb014c939ce74b86ab5d
Matthieu Bournat <matthieu.bournat@modisfrance.fr>
2019-01-14 17:09:13 +0100

    Fetch a channel-#2

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ function handleSignoutClick(event) {
 }
 
 /**
- * Print files.
+ * Get Channel informations
  */
 function getChannel(channel) {
     gapi.client.youtube.channels.list({
@@ -91,13 +91,40 @@ function getChannel(channel) {
         console.log(response);
         const channel = response.result.items[0];
 
-        const output = `
-            <ul class="collection>
-                <li class="collection-item">ID: ${channel.id}</li>
-            </ul>
-        `;
+        // Build the output to display channel datas 
+        const output = buildChannelTemplate(channel);
+
+        // Display the channel informations
+        showChannelData(output);
     })
     .catch(err => 
         alert('No channel by that name : ' + channel)
     );
+}
+
+/**
+ * Build channel template.
+ */
+function buildChannelTemplate(channel) {
+    return `
+        <ul class="collection>
+            <li class="collection-item">Title: ${channel..snippet.title}</li>
+            <li class="collection-item">ID: ${channel.id}</li>
+            <li class="collection-item">Subscribers: ${channel.statistics.subscriberCount}</li>
+            <li class="collection-item">Views: ${channel.statistics.viewCount}</li>
+            <li class="collection-item">Videos: ${channel.statistics.videoCount}</li>
+        </ul>
+        <p>${channel.snippet.description}</p>
+        <hr>
+        <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${channel.snippet.customUrl}">Visit Channel</a>
+    `;
+}
+
+/**
+ * Display channel datas.
+ */
+function showChannelData(data) {
+    const channelData = document.getElementById('channel-data');
+
+    channelData.innerHTML = data;
 }
